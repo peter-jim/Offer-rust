55d0ff7fbbe2ddc1fb03939dc1ac9a0d2a4a451b
peter-jim <835440306@qq.com>
2021-10-11 17:40:29 +0800

    update

--- finde_repeate_number/src/main.rs
+++ finde_repeate_number/src/main.rs
@@ -1,3 +1,56 @@
+use core::num;
+use std::collections::HashSet;
+struct Solution{
+
+}
+
+impl Solution {
+    
+
+    //
+    pub fn find_repeate_number(nums: Vec<i32>) -> i32 {
+        /*
+        由于对语法的不熟悉,用for很不友好
+        */
+        let mut n =0;
+        while n<nums.len() {
+            let mut j = n + 1 ;
+            while j < nums.len() {
+               if nums[n] == nums[j]{
+                //println!("{}",nums[n])
+               return  nums[n];
+              } 
+                j=j+1;
+            }
+            n=n+1;
+        }
+        0
+    }
+
+    pub fn find_repeate_number2(nums: Vec<i32>) -> i32 {
+        let mut store: HashSet<i32> = HashSet::new();
+        let mut res = -1;
+        
+                for i in 0..nums.len() {
+                    if store.contains(&nums[i]) {
+                        res = nums[i];
+                        return res;
+                        
+                    }
+                    else {
+                        store.insert(nums[i]);
+                    }
+                }
+        
+                for i in store.into_iter(){
+                    println!("{}" , &i)
+                }
+        
+        res
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    
+    Solution::find_repeate_number2([1,1,2,3,4,2,2].to_vec());
 }
